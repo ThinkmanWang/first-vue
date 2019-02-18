b0faa0e75d4e850119b19c534a11fe04833076ce
Thinkman Wang <wangxf1985@gmail.com>
2019-02-18 13:49:48 +0800

    update

--- src/components/First.vue
+++ src/components/First.vue
@@ -12,18 +12,40 @@
         <button v-on:click="show = !show">
             Toggle
         </button>
+        <br/>
         <transition name="fade">
             <p v-if="show">hello</p>
         </transition>
+        <br/>
+
+        <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+
+        <el-dialog
+                title="提示"
+                :visible.sync="dialogVisible"
+                width="30%"
+                :before-close="handleClose">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+          </span>
+        </el-dialog>
     </div>
 </template>
 
 <script type="text/javascript">
 
     import {EventBus} from "@/event-bus";
+    // import ElementUI from 'element-ui'
 
     export default {
         name: "First"
+        , data() {
+            return {
+                dialogVisible: false
+            };
+        }
         , props: {
             message: {
                 default: "FXXXXXK"
@@ -43,6 +65,13 @@
             }
             , incr() {
                 this.counter = Number(this.counter) + 1;
+            }
+            , handleClose(done) {
+                this.$confirm('确认关闭？')
+                    .then(_ => {
+                        done();
+                    })
+                    .catch(_ => {});
             }
         }
         // , data: {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-default/index.css'
+
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 // Vue.prototype.$EventBus = new Vue();
 
